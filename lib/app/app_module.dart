5562042ad99ea64936748e9dd3d498ca7008400d
Juniorlpes <bento.lopes50@gmail.com>
2020-07-30 10:59:12 -0300

    finished structure

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,3 @@
-  import 'package:heat_map/app/app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:heat_map/app/app_widget.dart';
@@ -6,7 +5,7 @@ import 'package:heat_map/app/modules/home/home_module.dart';
 
 class AppModule extends MainModule {
   @override
-  List<Bind> get binds => [Bind((i) => AppController()),];
+  List<Bind> get binds => [];
 
   @override
   List<Router> get routers => [
