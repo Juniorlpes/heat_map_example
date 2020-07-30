3b66fc0ca920bc04115ffb7aab2a6955b23de22a
Juniorlpes <bento.lopes50@gmail.com>
2020-07-30 13:06:00 -0300

    basic uxcam

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_uxcam/flutter_uxcam.dart';
+import 'package:heat_map/ux_key.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FlutterUxcam.optIntoSchematicRecordings();
+    FlutterUxcam.startWithKey(uxKey);
     return MaterialApp(
       navigatorKey: Modular.navigatorKey,
       title: 'Heat Map Example',
